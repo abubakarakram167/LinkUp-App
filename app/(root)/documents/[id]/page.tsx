f901325d30b2c3a59aaf5fa749a1303c5d770102
abubakar167 <abubakarakram167@gmail.com>
2024-08-22 02:39:48 +0500

    Add authentication on the app.

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Editor } from "@/components/editor/Editor";
 import Header from "@/components/Header";
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
 const Document = () => {
 	return (
@@ -9,6 +10,12 @@ const Document = () => {
 				<div className="flex w-fit items-center justify-center gap-2">
 					<p className="document-title"> This is a fake document title. </p>
 				</div>
+				<SignedOut>
+					<SignInButton />
+				</SignedOut>
+				<SignedIn>
+					<UserButton />
+				</SignedIn>
 			</Header>
 			<Editor />
 		</div>
