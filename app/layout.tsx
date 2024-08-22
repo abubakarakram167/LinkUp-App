f1fbf35d9e4562eacd22407faa5d5c6fd332d035
abubakar167 <abubakarakram167@gmail.com>
2024-08-22 21:52:30 +0500

    Add authentication and live collaboration server setup.

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { cn } from "@/lib/utils";
 import { Metadata } from "next";
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
+import Provider from "./Provider";
 
 const fontSans = FontSans({
 	subsets: ["latin"],
@@ -28,14 +29,13 @@ export default function RootLayout({
 			}}
 		>
 			<html lang="en" suppressHydrationWarning>
-				<head />
 				<body
 					className={cn(
 						"min-h-screen font-sans antialiased",
 						fontSans.variable
 					)}
 				>
-					{children}
+					<Provider>{children}</Provider>
 				</body>
 			</html>
 		</ClerkProvider>
